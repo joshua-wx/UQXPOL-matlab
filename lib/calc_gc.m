92361581d2ce45a35c4dcf19442a65603d5c0a07
andlowry <andlowry@gmail.com>
2016-03-17 16:35:42 +0100

    Update and move gc_calc.m

--- lib/calc_gc.m
+++ lib/calc_gc.m
@@ -13,12 +13,11 @@ function radar_struct = calc_gc(radar_struct)
 
 %REFS
 % Based on Rico-Ramirez et al. 2008 and Gourley et al. 2007
-% requires matlab simulink!
 
-nds_gc_ffn     = '../etc/nds_GC.mat';
-nds_precip_ffn = '../etc/nds_precip.mat';
-noise_mean_ffn = '../etc/noise_mean.mat';
-snr_level      = 10;
+nds_gc_ffn     = '../etc/nds_GC.mat';%'Testing/ClearAir/20151221/nds_GC.mat';%
+nds_precip_ffn = '../etc/nds_precip.mat';%'Testing/ClearAir/20151221/nds_precip.mat';%
+noise_mean_ffn = '../etc/noise_mean.mat';%'noise_mean_501.mat';
+snr_level      = 5;
 
 %% Load nds function data
 load(nds_gc_ffn);
@@ -39,12 +38,12 @@ fvel_nds_pre  = out_data.fvel;
 
 %% Define density function vectors
 
-xszdr = 0:0.01:10; %range of values for std(zdr) density function
-xszhh = 0:0.01:30; %range of values for std(zhh) density function
-xsphi = 0:0.01:150;%range of values for std(phi) density function
-xsrho = 0:0.01:1;  %range of values for std(rho) density function
-xrho  = 0:0.01:1.2; %range of values for rho density function
-xvel  = -5:0.01:5;  %range of values for vel density function
+xszdr=0:0.01:15; %range of values for std(zdr) density function
+xszhh=0:0.01:30; %range of values for std(zhh) density function
+xsphi=0:0.01:50;%range of values for std(phi) density function
+xsrho=0:0.001:1;  %range of values for std(rho) density function
+xvel=-5:0.01:5;  %range of values for vel density function
+xrho=0:0.001:1.2; %range of values for rho density function
 
 %% Calculate area under non-precip and precip curve
 % the grey area in Fig 3 from Gourley et al. 2007
@@ -72,27 +71,34 @@ display('check timing for possible preallocation')
 %% Load data from struct
 
 azi = radar_struct.data2.data;
-elv = radar_struct.data3.data;
+elv = radar_struct.data3.data; %not used - yet
 zhh = radar_struct.data5.data;
 vel = radar_struct.data6.data;
 zdr = radar_struct.data7.data;
 phi = radar_struct.data9.data;
 rho = radar_struct.data10.data;
 
-data_size = size(zhh);
+% data_size = size(zhh); %not used
 
 %load custom noise calibration file
 noise_mean=load(noise_mean_ffn); noise_mean=noise_mean.data;
+% test size(zhh) = size(noise_mean)
+if size(zhh,2) < size(noise_mean,2)
+    % shrink noise_mean to size(zhh)
+    noise_mean=noise_mean(1:size(zhh,2));
+elseif size(zhh,2) > size(noise_mean,2)
+    % pad noise_mean to sizse(zhh)
+    noise_mean=[noise_mean,nan(1,size(zhh,2)-size(noise_mean,2))];
+end
 SNR=zhh-repmat(noise_mean,size(zhh,1),1);
 snr_thres=SNR>=snr_level; %set logical of pixels to keep
 
-%check gate resultion to adjust no_gates below (need to be 3deg x 1.1km)
-
 %% Determine neighbourhood 3deg x 1.1km
+gate_res=radar_struct.header.gate_res;
+no_gates=1100./gate_res; %must be odd, relies on gate_res=100m
 d_azi=abs(nanmedian(diff(azi)));
-no_gates=11; %must be odd, relies on gate_res=100m
 no_azi= floor(3/d_azi);
-N=no_gates*no_azi;
+% N=no_gates*no_azi; %not used
 if mod(no_azi,2)== 0 %test if even
     no_azi=no_azi+1; %set to odd
 end
@@ -100,50 +106,52 @@ nhood=ones(no_azi,no_gates); %neighbourhood of 3deg x 1.1km
 %% Calculate additional fields
 % std(zdr), std(zhh), std(phi), std(rho)
 
-% Do convolution by ignoring nan's in calculation, see nanconv.m
-% Calculate standard deviation in nhood using sigma=sqrt[(1/n-1)*(sum(x^2) - (sum(x)^2/n))]
+% Calculate standard deviation in nhood using 
+% sigma=sqrt[(1/n-1)*(sum(x^2) - n*((sum(x)/n)^2))]
 
 % variables for all data types
 N_div=conv2(double(snr_thres),nhood,'same'); %calculate number of non-nan values for each nhood
-on=ones(data_size); %create matrix of ones
-on(~snr_thres)=0; %set nans to 0
-flat=conv2(on,nhood,'same');
+
 % zhh
-zhh_use=zhh; %zhh_use(~snr_thres)=nan;
+zhh_use=zhh; 
 zhh_use(~snr_thres)=0;
 zhh_use2=zhh_use.*zhh_use; %zhh^2
-zhc=conv2(zhh_use,nhood,'same')./flat;
-zhc(~snr_thres)=nan; %figure; imagesc(c); colorbar; title('c'); %zhh_use2(~snr_thres)=0;
-zhc2=conv2(zhh_use2,nhood,'same')./flat;
-zhc2(~snr_thres)=nan; %figure; imagesc(c2); colorbar; title('c2');
-zhh_std=sqrt((zhc2-(zhc.^2)./N_div)./(N_div-1)); 
+zhc=conv2(zhh_use,nhood,'same');
+zhc(~snr_thres)=nan; 
+zhc2=conv2(zhh_use2,nhood,'same');
+zhc2(~snr_thres)=nan; 
+zhh_std=sqrt((zhc2-N_div.*(zhc./N_div).^2)./(N_div-1)); 
 % zdr
 zdr_use=zdr;
 zdr_use(~snr_thres)=0;
 zdr_use2=zdr_use.*zdr_use; %zdr^2
-zdc=conv2(zdr_use,nhood,'same')./flat;
+zdc=conv2(zdr_use,nhood,'same');
 zdc(~snr_thres)=nan; 
-zdc2=conv2(zdr_use2,nhood,'same')./flat;
+zdc2=conv2(zdr_use2,nhood,'same');
 zdc2(~snr_thres)=nan; 
-zdr_std=sqrt((zdc2-(zdc.^2)./N_div)./(N_div-1));
+zdr_std=sqrt((zdc2-N_div.*(zdc./N_div).^2)./(N_div-1));
 % phidp
 phi_use=phi;
+[~,phi_ind]=max(~isnan(phi_use),[],2); %get the col index of the first non-nan value of phidp_grid. Returns m x 1 vector
+phi_idx=sub2ind(size(phi_use),(1:size(phi_use,1))',phi_ind); %get the linear index of above
+phi_use=bsxfun(@minus,phi_use,phi_use(phi_idx));%reset initial phi to zero
+phi_use=max(phi_use,0); %make sure all phi > 0
 phi_use(~snr_thres)=0;
 phi_use2=phi_use.*phi_use; %phi^2
-pc=conv2(phi_use,nhood,'same')./flat;
+pc=conv2(phi_use,nhood,'same');
 pc(~snr_thres)=nan; 
-pc2=conv2(phi_use2,nhood,'same')./flat;
+pc2=conv2(phi_use2,nhood,'same');
 pc2(~snr_thres)=nan; 
-phi_std=sqrt((pc2-(pc.^2)./N_div)./(N_div-1));
+phi_std=sqrt((pc2-N_div.*(pc./N_div).^2)./(N_div-1));
 % rhohv
 rho_use=rho;
 rho_use(~snr_thres)=0;
 rho_use2=rho_use.*rho_use; %rho^2
-rc=conv2(rho_use,nhood,'same')./flat;
+rc=conv2(rho_use,nhood,'same');
 rc(~snr_thres)=nan; 
-rc2=conv2(rho_use2,nhood,'same')./flat;
+rc2=conv2(rho_use2,nhood,'same');
 rc2(~snr_thres)=nan; 
-rho_std=sqrt((rc2-(rc.^2)./N_div)./(N_div-1));
+rho_std=sqrt((rc2-N_div.*(rc./N_div).^2)./(N_div-1));
 
 %% Calculate the membership value for each pixel. That is for each pixel 
 % calculate the f(x) for this pixel (actually just look it up) ,where f(x) 
@@ -152,44 +160,56 @@ rho_std=sqrt((rc2-(rc.^2)./N_div)./(N_div-1));
 % szdr
 zdr_std_use=zdr_std;
 zdr_std_use(isnan(zdr_std))=0; %set nan = 0
-fszdr_gc=fixpt_interp1(xszdr,fszdr_nds_gc,zdr_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+zdr_std_use=min(zdr_std_use,max(xszdr)); %need to set upper bound
+zdr_std_use=max(zdr_std_use,min(xszdr)); %need to set lower bound
+fszdr_gc=interp1(xszdr,fszdr_nds_gc,zdr_std_use,'spline'); 
 fszdr_gc(isnan(zdr_std))=nan; %set 0 = nan
-fszdr_pre=fixpt_interp1(xszdr,fszdr_nds_pre,zdr_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+fszdr_pre=interp1(xszdr,fszdr_nds_pre,zdr_std_use,'spline');
 fszdr_pre(isnan(zdr_std))=nan; %set 0 = nan
 % szhh
 zhh_std_use=zhh_std;
 zhh_std_use(isnan(zhh_std))=0; %set nan = 0
-fszhh_gc=fixpt_interp1(xszhh,fszhh_nds_gc,zhh_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+zhh_std_use=min(zhh_std_use,max(xszhh)); %need to set upper bound
+zhh_std_use=max(zhh_std_use,min(xszhh)); %need to set lower bound
+fszhh_gc=interp1(xszhh,fszhh_nds_gc,zhh_std_use,'spline'); 
 fszhh_gc(isnan(zhh_std))=nan; %set 0 = nan
-fszhh_pre=fixpt_interp1(xszhh,fszhh_nds_pre,zhh_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+fszhh_pre=interp1(xszhh,fszhh_nds_pre,zhh_std_use,'spline'); 
 fszhh_pre(isnan(zhh_std))=nan; %set 0 = nan
 % sphi
 phi_std_use=phi_std;
 phi_std_use(isnan(phi_std))=0; %set nan = 0
-fsphi_gc=fixpt_interp1(xsphi,fsphi_nds_gc,phi_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+phi_std_use=min(phi_std_use,max(xsphi)); %need to set upper bound
+phi_std_use=max(phi_std_use,min(xsphi)); %need to set lower bound
+fsphi_gc=interp1(xsphi,fsphi_nds_gc,phi_std_use,'spline'); 
 fsphi_gc(isnan(phi_std))=nan; %set 0 = nan
-fsphi_pre=fixpt_interp1(xsphi,fsphi_nds_pre,phi_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+fsphi_pre=interp1(xsphi,fsphi_nds_pre,phi_std_use,'spline'); 
 fsphi_pre(isnan(phi_std))=nan; %set 0 = nan
 % srho
 rho_std_use=rho_std;
 rho_std_use(isnan(rho_std))=0; %set nan = 0
-fsrho_gc=fixpt_interp1(xsrho,fsrho_nds_gc,rho_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+rho_std_use=min(rho_std_use,max(xsrho)); %need to set upper bound
+rho_std_use=max(rho_std_use,min(xsrho)); %need to set lower bound
+fsrho_gc=interp1(xsrho,fsrho_nds_gc,rho_std_use,'spline'); 
 fsrho_gc(isnan(rho_std))=nan; %set 0 = nan
-fsrho_pre=fixpt_interp1(xsrho,fsrho_nds_pre,rho_std_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+fsrho_pre=interp1(xsrho,fsrho_nds_pre,rho_std_use,'spline'); 
 fsrho_pre(isnan(rho_std))=nan; %set 0 = nan
 % rho
 rho_use1=rho; rho_use1(~snr_thres)=nan;
 rho_use1(isnan(rho))=0; %set nan = 0
-frho_gc=fixpt_interp1(xrho,frho_nds_gc,rho_use1,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+rho_use1=min(rho_use1,max(xrho)); %need to set upper bound
+rho_use1=max(rho_use1,min(xrho)); %need to set lower bound
+frho_gc=interp1(xrho,frho_nds_gc,rho_use1,'spline'); 
 frho_gc(isnan(zdr_std))=nan; %set 0 = nan
-frho_pre=fixpt_interp1(xrho,frho_nds_pre,rho_use1,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+frho_pre=interp1(xrho,frho_nds_pre,rho_use1,'spline');
 frho_pre(isnan(zdr_std))=nan; %set 0 = nan
 % vel
 vel_use=vel; vel_use(~snr_thres)=nan;
 vel_use(isnan(vel))=0; %set nan = 0
-fvel_gc=fixpt_interp1(xvel,fvel_nds_gc,vel_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+vel_use=min(vel_use,max(xvel)); %need to set upper bound
+vel_use=max(vel_use,min(xvel)); %need to set lower bound
+fvel_gc=interp1(xvel,fvel_nds_gc,vel_use,'spline'); 
 fvel_gc(isnan(vel))=nan; %set 0 = nan
-fvel_pre=fixpt_interp1(xvel,fvel_nds_pre,vel_use,sfix(8),2^-3,sfix(16),2^-14,'Floor');
+fvel_pre=interp1(xvel,fvel_nds_pre,vel_use,'spline'); 
 fvel_pre(isnan(vel))=nan; %set 0 = nan
 
 % This should produce 2x6 values for the 2 classes GC, Precip.
@@ -222,8 +242,9 @@ gc=Q2>Q1;  % ground clutter
 
 rho_rej=rho<0.7; %reject any pixel with rho < 0.7 as ~ precip
 pre(rho_rej)=0;
-phi_rej=phi<179; %reject any pixel with phi < 179 as ~ precip
-pre(phi_rej)=0;
+% the phi test is causing issues - so remove it for now, maybe fix after we get raw phi_dp
+% phi_rej=phi<179; %reject any pixel with phi < 179 as ~ precip
+% pre(phi_rej)=0;
 zhh_rej=zhh<5; %reject any pixel with zhh < 5 as ~ precip
 pre(zhh_rej)=0;
 sphi_rej=phi_std>100; % reject any pixel with std(phi) > 100 as ~ precip
